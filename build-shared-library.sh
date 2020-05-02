ac1d406b3f91080d4dc403a315ce09dc0bb61567
webfolderio <support@webfolder.io>
2020-05-03 00:54:02 +0300

    fixed [skip ci]

--- build-shared-library.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp CMakeLists.root.lib akumuli/CMakeLists.txt
-cp CMakeLists.lib-shared akumuli/libakumuli/CMakeLists.txt
-
-./build-akumuli.sh
-
-mkdir -p $BUILD_DIR/dist/shared-lib
-cp -P akumuli/build/libakumuli/libakumuli.* $BUILD_DIR/dist/shared-lib
--- build-static-library.sh
+++ build-static-library.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p $BUILD_DIR/dist/static-lib
-cp akumuli/build/libakumuli/libakumuli.a $BUILD_DIR/dist/static-lib
+mkdir -p $BUILD_DIR/dist/lib
+cp akumuli/build/libakumuli/*.a $BUILD_DIR/dist/lib
 
 mkdir -p $BUILD_DIR/dist/include $BUILD_DIR/dist/include
 cp akumuli/include/akumuli_version.h $BUILD_DIR/dist/include
